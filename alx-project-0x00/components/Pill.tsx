1a1c98d34dd88efec16e54118ed86ec27e357eb5
Nkeiru Lois <nkeiruloisy@yahoo.com>
2025-11-16 01:58:42 +0100

    phil component implemented

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,4 +1,7 @@
-const Pill: React.FC<{ title: string }> = ({ title }) => {
+import React from "react";
+import { PillProps } from "@/interfaces";
+
+const Pill: React.FC<PillProps> = ({ title }) => {
   return (
     <div className="px-2 py-1 bg-gray-200 rounded-full text-sm font-medium">
       {title}
